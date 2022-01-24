bbe74110219b9bc1e826c26e16c5f7edb2198493
Jack <hyojong6367@gmail.com>
2022-01-24 10:46:20 +0900

    Update examples

--- Example/SwiftUIWheelPicker/ViewController.swift
+++ Example/SwiftUIWheelPicker/ViewController.swift
@@ -11,162 +11,183 @@ import SwiftUI
 import SwiftUIWheelPicker
 
 struct MainView: View {
-    @State var index: Int = 5
-    @State var index2: Int = 5
-    @State var index3: Int = 5
-    @State var index4: Int = 5
-    @State var index5: Int = 5
-    @State var index6: Int = 5
-    @State var index7: Int = 5
-    @State var index8: Int = 5
+    @State var indexBasic: Int = 5
+    @State var indexRatio: Int = 5
+    @State var indexAlpha: Int = 5
+    @State var indexScale: Int = 5
+    @State var indexGradient: Int = 5
+    @State var indexIndicator: Int = 5
+    @State var indexValue: Int = 5
+    @State var indexScrollTo: Int = 5
     @State var items: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     var body: some View {
-        //        NavigationView {
-        //            List {
-        //                NavigationLink(destination: BasicUseView()) {
-        //                    Text("Basic use")
-        //                }
-        //                NavigationLink(destination: WithIndicatorView()) {
-        //                    Text("With indicator")
-        //                }
-        //                NavigationLink(destination: WithAnimationView()) {
-        //                    Text("With animation")
-        //                }
-        //            }
-        //            .navigationBarTitle("Home")
-        //        }
         GeometryReader { geometry in
-            VStack(alignment: .center, spacing: 0) {
-                SwiftUIWheelPicker($index, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                            .background(value % 2 == 0 ? Color.orange : Color.yellow)
+            ScrollView(.vertical, showsIndicators: true) {
+                VStack(alignment: .leading, spacing: 0) {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Basic").font(Font.title).padding(.top, 20)
+                        SwiftUIWheelPicker($indexBasic, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .padding(.top, 10)
                     }
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack(alignment: .bottom, spacing: 0) {
+                            Text("With Visible Count ").font(Font.title)
+                            Text("(Ratio)").font(Font.body)
+                        }
+                        SwiftUIWheelPicker($indexRatio, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .width(.Ratio(0.1))
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("With Alpha").font(Font.title)
+                        SwiftUIWheelPicker($indexAlpha, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .width(.VisibleCount(7))
+                        .scrollAlpha(0.1)
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("With Scale").font(Font.title)
+                        SwiftUIWheelPicker($indexScale, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .width(.VisibleCount(7))
+                        .scrollScale(0.4)
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("With Gradient").font(Font.title)
+                        SwiftUIWheelPicker($indexGradient, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .width(.Fixed(40))
+                        .edgeLeft(AnyView(
+                            LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.4), Color.white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
+                        ), width: .Ratio(0.2))
+                        .edgeRight(AnyView(
+                            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0), Color.gray.opacity(0.4)]), startPoint: .leading, endPoint: .trailing)
+                        ), width: .Ratio(0.2))
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Center indicator").font(Font.title)
+                        SwiftUIWheelPicker($indexIndicator, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                            }
+                        }
+                        .width(.Fixed(40))
+                        .centerView(AnyView(
+                            HStack(alignment: .center, spacing: 0) {
+                                Divider()
+                                    .frame(width: 1)
+                                    .background(Color.gray)
+                                    .padding(EdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0))
+                                    .opacity(0.4)
+                                Spacer()
+                                Divider()
+                                    .frame(width: 1)
+                                    .background(Color.gray)
+                                    .padding(EdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0))
+                                    .opacity(0.4)
+                            }
+                        ), width: .Fixed(40))
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Selected Value : \(indexValue)").font(Font.title)
+                        SwiftUIWheelPicker($indexValue, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                                    .background(value % 2 == 0 ? Color.blue.opacity(0.4) : Color.green.opacity(0.4))
+                            }
+                        }
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .border(Color.black, width: 1)
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack(alignment: .bottom, spacing: 20) {
+                            Text("Scroll To ").font(Font.title)
+                            Spacer()
+                            Button("First") {
+                                indexScrollTo = 0
+                            }
+                            Button("Center") {
+                                indexScrollTo = items.count / 2
+                            }
+                            Button("Last") {
+                                indexScrollTo = items.count - 1
+                            }
+                        }
+                        SwiftUIWheelPicker($indexScrollTo, items: $items) { value in
+                            GeometryReader { reader in
+                                Text("\(value)")
+                                    .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
+                                    .background(value % 2 == 0 ? Color.blue.opacity(0.4) : Color.green.opacity(0.4))
+                            }
+                        }
+                        .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 30)
                 }
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .border(Color.black, width: 1)
-                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index2, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                            .background(value % 2 == 0 ? Color.orange : Color.yellow)
-                    }
-                }
-                .width(.Fixed(20))
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index3, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                            .background(value % 2 == 0 ? Color.orange : Color.yellow)
-                    }
-                }
-                .width(.VisibleCount(3))
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index4, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                            .background(value % 2 == 0 ? Color.orange : Color.yellow)
-                    }
-                }
-                .width(.Fixed(30))
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .border(Color.black, width: 1)
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index5, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                    }
-                }
-                .width(.VisibleCount(7))
-                .scrollAlpha(0.2)
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index6, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                    }
-                }
-                .width(.VisibleCount(7))
-                .scrollScale(0.4)
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index7, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                    }
-                }
-                .width(.Fixed(40))
-                .edgeLeft(AnyView(
-                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.8), Color.white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
-                ), width: .Ratio(0.2))
-                .edgeRight(AnyView(
-                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0), Color.white.opacity(0.8)]), startPoint: .leading, endPoint: .trailing)
-                ), width: .Ratio(0.2))
-                .scrollAlpha(0.2)
-                .scrollScale(0.5)
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
-                
-                SwiftUIWheelPicker($index8, items: $items) { value in
-                    GeometryReader { reader in
-                        Text("\(value)")
-                            .frame(width: reader.size.width, height: reader.size.height, alignment: .center)
-                    }
-                }
-                .width(.Fixed(40))
-                .edgeLeft(AnyView(
-                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.8), Color.white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
-                ), width: .Ratio(0.2))
-                .edgeRight(AnyView(
-                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0), Color.white.opacity(0.8)]), startPoint: .leading, endPoint: .trailing)
-                ), width: .Ratio(0.2))
-                .centerView(AnyView(
-                    HStack(alignment: .center, spacing: 0) {
-                        Divider()
-                            .frame(width: 1)
-                            .background(Color.gray)
-                            .padding(EdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0))
-                            .opacity(0.4)
-                        Spacer()
-                        Divider()
-                            .frame(width: 1)
-                            .background(Color.gray)
-                            .padding(EdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0))
-                            .opacity(0.4)
-                    }
-                ), width: .Fixed(40))
-                .scrollAlpha(0.2)
-                .scrollScale(0.5)
-                .frame(width: geometry.size.width - 40, height: 40, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2.0))
-                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 40, trailing: 20))
             }
         }
     }
